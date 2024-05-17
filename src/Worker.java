e195461a44411a3a5f1ee1a6f10cf8e6e4824c70
Mohammed Alam <efternamn.fornamn@elev.ga.ntig.se>
2024-05-17 21:43:07 +0200

    bas klassen worker och konstruktor som tar emot namn och ålder

---
+++ src/Worker.java
@@ -0,0 +1,13 @@
+public class Worker {
+
+    protected String name;
+    protected int age;
+    protected double wage;
+    public Worker(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.wage = 0; // Initialisera wage till 0
+    }
+
+}
+
