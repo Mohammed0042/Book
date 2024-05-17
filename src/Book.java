a4e442a45e0b146d6dcddf5e824252004827b022
Mohammed Alam <efternamn.fornamn@elev.ga.ntig.se>
2024-05-17 20:46:47 +0200

    Har lagt till currentPage till Book klassen och värdet är 0. Turnpage är sin egen metod och det ökar current page med 1

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Book {
+public class Book {
+    public String Name;
+    public int Pages;
+    public int currentPage;
+
+    public Book(String Name,int Pages){
+        this.Name = Name;
+        this.Pages = Pages;
+        this.currentPage = 0;
+    }
+public void TurnPage(){
+        if (currentPage < Pages) {
+            currentPage++;
+        }
+        else {
+            System.out.println("Du har läst alla sidor");
+        }
+        }
+
+        public static void main(String[]args) {
+
+            Book book1 = new Book("Diary Of A Wimpy Kid", 300);
+            Book book2 = new Book("Harry Potter", 400);
+            Book book3 = new Book("The Walking Dead", 200);
+
+            System.out.println("Current Page För Diary Of A Wimpy Kid = " + book1.currentPage);
+
+            book1.TurnPage();
+            book1.TurnPage();
+
+            System.out.println("Current Page För Diary Of A Wimpy Kid");
+
+        }
 }
