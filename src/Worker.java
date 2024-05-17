395d2b83894ca28a6f050ac12ed92d9366556019
Mohammed Alam <efternamn.fornamn@elev.ga.ntig.se>
2024-05-17 21:44:46 +0200

    olika metoder för att få värdet på wage,age,name

--- src/Worker.java
+++ src/Worker.java
@@ -8,6 +8,19 @@ public class Worker {
         this.age = age;
         this.wage = 0; // Initialisera wage till 0
     }
+    public void SetWage(double wage) {
+        this.wage = wage;
+    }
+    public String GetName() {
+        return name;
+    }
+    public int GetAge() {
+        return age;
+    }
+    public double GetWage() {
+        return wage;
+    }
+
 
 }
 
